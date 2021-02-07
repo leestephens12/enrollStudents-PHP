966e5c6a2070de5a9afbe39e2dcc47623cdfb745
leestephens12 <77406283+leestephens12@users.noreply.github.com>
2021-02-07 15:17:42 -0500

    HTML form method complete(Still have to created dropdown meny with PHP)

--- index.php
+++ index.php
@@ -2,8 +2,27 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>Add New Student</title>
     </head>
     <body>
+        <header> <!--Header for page-->
+            <h1>Add a New Student</h1>
+        </header>
+        <main><!--Main section for page-->
+            <!--Form method passing inputed information to database-->
+            <form method="post" action="student-info">
+                <!--Input section for fist name-->
+                <label for="firstName">First Name:</label>
+                <input type="text" id="firstName" name="firstName"><br /><br />
+                <!--Input section for last name-->
+                <label for="lastName">Last Name:</label>
+                <input type="text" id="lastName" name="lastName"><br /><br />
+                <!--Input section for student number-->
+                <label for="studentNumber">Student Number:</label>
+                <input type="number" id="studentNumber" name="studentNumber"><br /><br />
+                <!--Button to submit all information-->
+                <input type="submit" value="submit">
+            </form>
+        </main>
     </body>
 </html>
